72e46ee5315ddcd68fd1f0759b36cb58ddbee32a
itsabhianant <71239642+itsabhianant@users.noreply.github.com>
2020-09-16 08:05:40 +0530

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,23 @@
+import random
+
+highest = 1000
+answer = random.randint(1, highest)
+
+guess = 1024        #you can use anything that's not b/w 1 and 1000
+
+print("Please guess a number between 1 and {}: ".format(highest))
+
+while guess != answer:
+    guess = int(input())
+
+    if guess == answer:
+        print("Well done, you guessed it ")
+        break
+    elif guess == 0:
+        print("GAME OVER")
+        break
+    else:
+        if guess < answer:
+            print("Please guess higher")
+        else:
+            print("Please guess lower")
